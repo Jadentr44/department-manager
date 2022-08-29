6e0e395b30b893e05b10c137be6a29649e8ff05a
Jaden <jadentr.44@gmail.com>
2022-08-29 00:41:30 -0500

    added connection to db

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const inquirer = require('inquirer');
-
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL Username
+    user: 'root',
+    // TODO: Add MySQL Password
+    password: '',
+    database: 'books_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
 inquirer
   .prompt([
     {
       type: 'list',
       name: 'option',
       message: 'What would you like to view?',
-      choices: ['all roles', 'crocodile'],
+      choices: ['all roles', 'all departments'],
     },
   ])
   .then(answers => {
-    console.info('Answer:', answers.reptile);
+    let userInput = answers.option
+
   });
